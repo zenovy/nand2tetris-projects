80283e4e59517245361b09577f94179769525459
Rory Sametz <rorysametz@gmail.com>
2018-07-26 09:44:53 -0700

    04 - Multiplication ASM Solution

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,28 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// Set output to 0
+@R2
+M=0
+(START)
+// Jump if R1 == 0
+@R1
+D=M
+@END
+D;JEQ
+// Set R0 to D
+@R0
+D=M
+// Add D to R2
+@R2
+M=M+D
+// Reduce R1
+@R1
+M=M-1
+// Jump to start
+@START
+0;JMP
+// End
+(END)
+@END
+0;JMP
